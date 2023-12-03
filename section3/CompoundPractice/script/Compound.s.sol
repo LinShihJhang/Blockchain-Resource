f6bedcb7f0a41d7e8fd488bc22810392e1e9812a
ShihChangLin <els910270@gmail.com>
2023-12-03 21:35:42 +0800

    add example

---
+++ section3/CompoundPractice/script/Compound.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+
+
+contract SimpleSwapScript is Script {
+    function run() external {
+        //vm.startBroadcast('private key');
+
+        address token0 = 0xb16F35c0Ae2912430DAc15764477E179D9B9EbEa;
+        address token1 = 0x51fCe89b9f6D4c530698f181167043e1bB4abf89;
+
+        SimpleSwap simpleSwap = new SimpleSwap(token0,token1);
+
+        //vm.stopBroadcast();
+    }
+}

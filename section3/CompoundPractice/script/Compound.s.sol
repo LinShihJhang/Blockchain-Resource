e774621f9fd076996e81f09f34de5c3f53d5300a
ShihChangLin <els910270@gmail.com>
2023-12-03 21:46:48 +0800

    add erc20

--- section3/CompoundPractice/script/Compound.s.sol
+++ section3/CompoundPractice/script/Compound.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
-
+import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 contract SimpleSwapScript is Script {
     function run() external {
@@ -11,7 +11,7 @@ contract SimpleSwapScript is Script {
         address token0 = 0xb16F35c0Ae2912430DAc15764477E179D9B9EbEa;
         address token1 = 0x51fCe89b9f6D4c530698f181167043e1bB4abf89;
 
-        SimpleSwap simpleSwap = new SimpleSwap(token0,token1);
+        //SimpleSwap simpleSwap = new SimpleSwap(token0, token1);
 
         //vm.stopBroadcast();
     }
